e18dccbafc150dc023aec8bbae017d9a2d8c8cc0
jerome.bulanadi@gmail.com <jerome.bulanadi@gmail.com@7c2a300d-4f0b-d38e-e480-129cc970396d>
2011-01-24 09:52:04 +0000

    Added compatibility with jQuery.noConflict()

--- javascripts/jquery-list-attributes.js
+++ javascripts/jquery-list-attributes.js
@@ -4,14 +4,14 @@ if(jQuery) {
 	jQuery(document).ready(function() {
 		jQuery.fn.listAttrs = function(prefix) {
 			var list = [];
-			var $elements = $(this);
+			var $elements = jQuery(this);
 			var size = $elements.size();
 			if (size == 0){
 				return list;
 			}
-			$($elements[0].attributes).each(function(index){
-				if($(this)[0].nodeValue != null && $(this)[0].nodeValue != '') {
-					list.push($(this)[0].nodeName);
+			jQuery($elements[0].attributes).each(function(index){
+				if(jQuery(this)[0].nodeValue != null && jQuery(this)[0].nodeValue != '') {
+					list.push(jQuery(this)[0].nodeName);
 				};
 			});
 			return list;
